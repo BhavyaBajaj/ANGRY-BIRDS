b4425f7e98de536ca83e5d05943ffdf6cf74cc4c
BhavyaBajaj <bhavyabajaj2006@gmail.com>
2021-06-08 13:52:16 +0530

    blueprint done

--- box.js
+++ box.js
@@ -18,7 +18,9 @@ class Box {
     translate(pos.x, pos.y);
     rotate(angle);
     rectMode(CENTER);
-    fill(255);
+    fill("green");
+    stroke("white");
+    strokeWeight(3)
     rect(0, 0, this.width, this.height);
     pop();
   }
--- sketch.js
+++ sketch.js
@@ -6,13 +6,24 @@ var engine, world;
 var box1;
 
 function setup(){
-    var canvas = createCanvas(400,400);
+    var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    box1 = new Box(200,300,50,50);
-    box2 = new Box(240,100,50,100);
-    ground = new Ground(200,height,400,20)
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3= new Box(700,240,70,70);
+    box4= new Box(920,240,70,70);
+    box5= new Box(810,160,70,70);
+    ground = new Ground(600,height,3000,20);
+    pig1=new Pig(810,320);
+    pig2=new Pig(810,240)
+    Log1= new Log(810,260,300,PI/2)
+    Log2= new Log(810,180,310,PI/2)
+    Log3= new Log(760,120,150,PI/7)
+    Log4= new Log(870,120,150,-PI/7)
+    bird= new Bird(100,100)
+
 }
 
 function draw(){
@@ -23,5 +34,15 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
     ground.display();
+    pig1.display()
+    pig2.display()
+    Log1.display();
+    Log2.display();
+    Log3.display();
+    Log4.display();
+    bird.display();
 }
